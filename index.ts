8d2f7b5daf0a4d3e91995ab81cdb26f8a6a72418
Gabriel Bueno <gazevedo@gmalato.com>
2021-12-28 19:20:53 -0300

    feat: change things to allow typescript

--- index.ts
+++ index.ts
@@ -1,18 +1,7 @@
 import Vehicle from "./models/Vehicle";
 import World from "./models/World";
 
-let w = new World();
-
-w.addVehicle(new Vehicle(10, 10, 0));
-
-const v: Vehicle = w.getVehicle(0);
-
-v.setSpeed(10);
-// v.move();
-
-// v.setAngleSpeed(90);
-// v.rotate();
-
-v.move();
-
-console.log("I have created a world!", w.getVehicle(0));
+export {
+	Vehicle,
+	World,
+}
